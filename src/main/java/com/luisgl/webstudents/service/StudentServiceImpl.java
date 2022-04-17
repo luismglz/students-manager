93b5537d43edee50d528afea8607c0b768396271
Luis Gonzalez <lmgl887@gmail.com>
2022-04-16 23:37:43 -0500

    add service implementation

---
+++ src/main/java/com/luisgl/webstudents/service/StudentServiceImpl.java
@@ -0,0 +1,15 @@
+package com.luisgl.webstudents.service;
+
+import com.luisgl.webstudents.entity.Student;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentServiceImpl implements StudentService{
+
+    @Override
+    public List<Student> getStudents() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
