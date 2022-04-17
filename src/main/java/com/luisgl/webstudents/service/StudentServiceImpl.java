28606ab9c94c0f23b16237077b0d9dd36610316b
Luis Gonzalez <lmgl887@gmail.com>
2022-04-16 23:50:56 -0500

    findAll in service impl

--- src/main/java/com/luisgl/webstudents/service/StudentServiceImpl.java
+++ src/main/java/com/luisgl/webstudents/service/StudentServiceImpl.java
@@ -1,15 +1,20 @@
 package com.luisgl.webstudents.service;
 
 import com.luisgl.webstudents.entity.Student;
+import com.luisgl.webstudents.repository.StudentRepository;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentServiceImpl implements StudentService{
 
+    @Autowired
+    private StudentRepository repo;
+    
     @Override
     public List<Student> getStudents() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return repo.findAll();
     }
     
 }
