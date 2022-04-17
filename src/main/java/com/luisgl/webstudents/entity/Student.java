cb0216eef425be5b2ef21965ef2155be817f272d
Luis Gonzalez <lmgl887@gmail.com>
2022-04-16 23:27:13 -0500

    add Student model

--- src/main/java/com/luisgl/webstudents/entity/Student.java
+++ src/main/java/com/luisgl/webstudents/entity/Student.java
@@ -1,13 +1,75 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.luisgl.webstudents.entity;
 
-/**
- *
- * @author lmgl8
- */
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import sun.jvm.hotspot.gc.shared.Generation;
+
+@Entity
+@Table(name = "student")
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+    
+    @Column(name = "lastname", nullable = false, length = 50)
+    private String lastname;
+    
+    @Column(name = "email", nullable = false, length = 50, unique = true)
+    private String email;
+    
+    public Student(){}
+
+    public Student(Long id, String name, String lastname, String email) {
+        this.id = id;
+        this.name = name;
+        this.lastname = lastname;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "id=" + id + ", name=" + name + ", lastname=" + lastname + ", email=" + email + '}';
+    }
+    
+    
     
 }
